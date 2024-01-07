4858124ab78fd611b7af7b01a42f5a84760dd4fc
MansoorSN <48001931+MansoorSN@users.noreply.github.com>
2024-01-06 23:58:12 -0500

    Update insta2.py

--- insta2.py
+++ insta2.py
@@ -2,11 +2,11 @@ from instagrapi import Client
 import streamlit as st
 
 cl = Client()
-cl.login("mansoords200", "Ottawa_22")
+cl.login(st.secrets.USERNAME, st.secrets.PASSWORD)
 
 post_url=st.text_input("Paste the insta url")
 
 post_id=cl.media_pk_from_url(post_url)
 
-if st.button:
+if st.button("Download Post"):
   cl.photo_download(post_id)
